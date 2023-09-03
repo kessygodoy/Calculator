8f13e91f25d0f278880cb409ef91c1181b7ff94d
kessygodoy <kessyon@gmail.com>
2023-09-03 09:54:35 -0300

    Lógica da Calculadora #01
    Criado logica de nao aceiar mais de um ponto ao mesmo tempo no display,  apaga o 0 padrao que fica na tela quando digita algum numero,

--- calculator/App.js
+++ calculator/App.js
@@ -1,47 +1,81 @@
-import React, {useState} from 'react';
-import {View, StyleSheet, TextInput, Text} from 'react-native';
+import React, { useState, Component } from 'react';
+import { View, StyleSheet, TextInput, Text } from 'react-native';
 import Button from './src/components/Button'
 import Display from './src/components/Display'
 
-export default () => {
-  state={
-    displayValue: '0'
-  }
-return(
-    <View style={style.container}>
-      <Display value={this.state.displayValue} />
-      <View style={style.buttons}>
-          <Button label='AC' />
-          <Button label='/' />
-          <Button label='7' />
-          <Button label='8' />
-          <Button label='9' />
-          <Button label='*' />
-          <Button label='4' />
-          <Button label='5' />
-          <Button label='6' />
-          <Button label='-' />
-          <Button label='1' />
-          <Button label='2' />
-          <Button label='3' />
-          <Button label='+' />
-          <Button label='0' />
-          <Button label='.' />
-          <Button label='=' />
-      </View>
-    </View>
-  )
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0,
+}
+export default class App extends Component {
+    state = { ...initialState }
+
+    addDigit = n => {
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false })
+
+        if (n !== '.') {
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[this.state.current] = newValue
+            this.setState({ values })
+        }
+    }
+
+    clearMemory = () => {
+        this.setState({ ...initialState })
+    }
+
+    setOperation = operation => {
+
+    }
+    render() {
+        return (
+            <View style={style.container}>
+                <Display value={this.state.displayValue} />
+                <View style={style.buttons}>
+                    <Button label='AC' triple onClick={this.clearMemory} />
+                    <Button label='/' operation onClick={this.setOperation} />
+                    <Button label='7' onClick={this.addDigit} />
+                    <Button label='8' onClick={this.addDigit} />
+                    <Button label='9' onClick={this.addDigit} />
+                    <Button label='*' operation onClick={this.setOperation} />
+                    <Button label='4' onClick={this.addDigit} />
+                    <Button label='5' onClick={this.addDigit} />
+                    <Button label='6' onClick={this.addDigit} />
+                    <Button label='-' operation onClick={this.setOperation} />
+                    <Button label='1' onClick={this.addDigit} />
+                    <Button label='2' onClick={this.addDigit} />
+                    <Button label='3' onClick={this.addDigit} />
+                    <Button label='+' operation onClick={this.setOperation} />
+                    <Button label='0' double onClick={this.addDigit} />
+                    <Button label='.' onClick={this.addDigit} />
+                    <Button label='=' operation onClick={this.setOperation} />
+                </View>
+            </View>
+        )
+    }
 };
 
 const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // padding: 22,
-  },
-  buttons: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  }
+    container: {
+        flex: 1,
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        // padding: 22,
+    },
+    buttons: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    }
 });
--- calculator/src/components/Button.js
+++ calculator/src/components/Button.js
@@ -9,20 +9,36 @@ import {
 const styles = StyleSheet.create({
     button: {
         fontSize: 40,
-        height: Dimensions.get('window').width/ 4,
+        height: Dimensions.get('window').width/ 4, //Pega o tamanho da tela e divide por 4 para definir o tamanho do botao
         width: Dimensions.get('window').width/ 4,
         padding: 20,
         backgroundColor: '#f0f0f0',
         textAlign: 'center',
         borderWidth: 1,
         borderColor: '#888'
+    },
+    operationButton: {
+        color: '#fff',
+        backgroundColor:'#fa8231',
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4) *2,
+    },
+    buttonTripple: {
+        width: (Dimensions.get('window').width / 4) *3,
     }
+
 })
 
 export default props => {
+    const stylesButton = [styles.button]
+
+    if(props.double) stylesButton.push(styles.buttonDouble)
+    if(props.triple) stylesButton.push(styles.buttonTripple)
+    if(props.operation) stylesButton.push(styles.operationButton)
     return(
-        <TouchableHighlight onPress={props.onClick}>
-            <Text style={styles.button}>{props.label}</Text>
+        <TouchableHighlight onPress={() => props.onClick(props.label)}>
+            <Text style={stylesButton}>{props.label}</Text>
         </TouchableHighlight>
     )
 }
